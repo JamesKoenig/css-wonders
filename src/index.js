588584ac37d0455aea3f199f95f3e2618a839b07
James Koenig <Koenig.James@Live.com>
2021-03-08 00:11:19 -0800

    fix scoping mistake (body needs enclose)
    silly mistake 😢

--- src/index.js
+++ src/index.js
@@ -6,22 +6,6 @@ const files = [
   "flex_direction.html",
 ].map( path => `assets/code_snippets/${path}` );
 
-const callbacks = [
-  response => {
-    let snippet = new CodeSnippet(response);
-    body.appendChild(snippet.renderText());
-    body.appendChild(snippet.render());
-  },
-  response => {
-    let snippet = new CodeSnippet(response);
-    body.appendChild(snippet.renderText());
-    body.appendChild(snippet.render());
-    body.appendChild(flexExampleButton("row"));
-    body.appendChild(flexExampleButton("row-reverse"));
-    body.appendChild(flexExampleButton("column"));
-    body.appendChild(flexExampleButton("column-reverse"));
-  },
-]
 
 document.addEventListener('DOMContentLoaded', () => {
   let body = document.getElementsByTagName("body")[0];
@@ -29,11 +13,28 @@ document.addEventListener('DOMContentLoaded', () => {
   let welcomeHeader = document.createElement("h1")
   welcomeHeader.innerText = "dynamically generated html!";
   body.appendChild(welcomeHeader);
+  const callbacks = [
+    response => {
+      let snippet = new CodeSnippet(response);
+      body.appendChild(snippet.renderText());
+      body.appendChild(snippet.render());
+    },
+    response => {
+      let snippet = new CodeSnippet(response);
+      body.appendChild(snippet.renderText());
+      body.appendChild(snippet.render());
+      body.appendChild(flexExampleButton("row"));
+      body.appendChild(flexExampleButton("row-reverse"));
+      body.appendChild(flexExampleButton("column"));
+      body.appendChild(flexExampleButton("column-reverse"));
+    },
+  ]
 
   files.map((filePath,idx) =>
     fetch(filePath)
       .then(x => x.text())
-      .then(response => callbacks[idx](response)));
+      .then(response => callbacks[idx](response))
+  );
 
   let button = document.createElement('button');
   button.innerText = "create other buttons";
